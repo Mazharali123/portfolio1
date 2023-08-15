4f52694822e5dd7c650c24b8630bacb34b5b5f6a
Mazhar Ali <92070943+Mazharali123@users.noreply.github.com>
2023-08-15 09:12:47 +0000

    Committed

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Mazhar Ali",
+  title: "Android Developer",
+  email: "mazhar.Ali@studentAmbassadors.com",
+  gitHub: "https://github.com/Mazharali123/",
+  //instagram: "",
+  linkedIn: "www.linkedin.com/in/mazharalishaikh  ",
+  //medium: "",
+  //twitter: "microsoft",
+  //youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
